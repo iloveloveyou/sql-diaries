66a0788517f3b283faa8746686f93a62be4146fd
Roberto Reale <roberto.reale82@gmail.com>
2014-06-06 15:11:06 +0200

    Sessions for Oracle database.

---
+++ oracle/sessions.sql
@@ -0,0 +1,21 @@
+--
+--  Sessions on the database
+--
+--  Tested on: 11g
+--
+
+
+SELECT
+    a.spid pid,
+    b.sid,
+    b.serial#,
+    b.machine,
+    b.username,
+    b.server,
+    b.osuser,
+    b.program
+FROM v$session b, v$process a
+WHERE
+    b.paddr = a.addr 
+    AND TYPE = 'USER'
+ORDER BY spid;
