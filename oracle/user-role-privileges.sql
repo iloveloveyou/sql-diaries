812740078f8b947178e84be227c15b42acf5cbb6
Roberto Reale <roberto.reale82@gmail.com>
2014-06-29 12:16:53 +0200

    Added three more queries.
    * System privileges for a user
    * Direct grants to tables/views
    * Indirect grants to tables/views

--- oracle/user-role-privileges.sql
+++ oracle/user-role-privileges.sql
@@ -14,3 +14,29 @@ SELECT lpad(' ', 2*LEVEL) || granted_role "Roles and privileges" FROM
     )
 START WITH grantee IS NULL CONNECT BY grantee = PRIOR granted_role
 /
+
+
+--  System privileges for a user
+
+SELECT PRIVILEGE
+  FROM sys.dba_sys_privs
+ WHERE grantee = <theUser>
+UNION
+SELECT PRIVILEGE 
+  FROM dba_role_privs rp JOIN role_sys_privs rsp ON (rp.granted_role = rsp.role)
+ WHERE rp.grantee = <theUser>
+ ORDER BY 1;
+
+--  Direct grants to tables/views
+
+SELECT owner, table_name, select_priv, insert_priv, delete_priv, update_priv, references_priv, alter_priv, index_priv 
+  FROM table_privileges
+ WHERE grantee = <theUser>
+ ORDER BY owner, table_name;
+
+--  Indirect grants to tables/views
+
+SELECT DISTINCT owner, table_name, PRIVILEGE 
+  FROM dba_role_privs rp JOIN role_tab_privs rtp ON (rp.granted_role = rtp.role)
+ WHERE rp.grantee = <theUser>
+ ORDER BY owner, table_name;
