9088c689a05f4cecd19d848486e158d50b35311a
Matias Monteverde Giannini <matias.monteverde@gmail.com>
2014-11-04 14:18:12 +0100

    Create priv_role_hierarchical_list.sql

---
+++ oracle/priv_role_hierarchical_list.sql
@@ -0,0 +1,10 @@
+
+
+
+set line 150
+select lpad(' ', 2*level) || granted_role "User, his roles and privileges" from (
+select null grantee, username granted_role from dba_users where username like upper('<USERNAME>')
+union select grantee, granted_role from dba_role_privs
+union select grantee, privilege from dba_sys_privs)
+start with grantee is null connect by grantee = prior granted_role
+/
