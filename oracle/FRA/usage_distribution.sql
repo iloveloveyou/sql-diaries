0b5be0ce9c8cc4f2ddabfc69f41a9ed2d983abea
Matias Monteverde Giannini <matias.monteverde@gmail.com>
2014-11-10 22:58:50 +0100

    Create usage_distribution.sql

---
+++ oracle/FRA/usage_distribution.sql
@@ -0,0 +1,16 @@
+
+
+
+
+-- natural number
+set line 200
+col name format a50
+
+select name, SPACE_LIMIT/1024/1024 as "Space Limit MBs", SPACE_USED/1024/1024 as "Space Used MBs", 
+SPACE_RECLAIMABLE/1024/1024 as "Space Usable MBs", NUMBER_OF_FILES as Files 
+from v$recovery_file_dest;
+
+-- Percentage
+
+set line 200
+select * from V_$RECOVERY_AREA_USAGE;
