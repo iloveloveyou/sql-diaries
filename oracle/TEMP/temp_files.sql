8402c70e0d820089e51f678c4c98b226069df5c0
Matias Monteverde Giannini <matias.monteverde@gmail.com>
2014-11-11 16:57:13 +0100

    Update and rename temp_files.sql to temp_queries.sql

--- oracle/TEMP/temp_files.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-
-set line 200
-col file_name format a80
-select file_name, tablespace_name, bytes/1024/1024 as "MBs" from dba_temp_files
-
---
+++ oracle/TEMP/temp_queries.sql
@@ -0,0 +1,16 @@
+
+
+
+-- List temp files
+set line 200
+col file_name format a80
+select file_name, tablespace_name, bytes/1024/1024 as "MBs" from dba_temp_files
+
+
+--- extend tempfiles
+ALTER DATABASE TEMPFILE '<+DATA4D/bkgcf1p/tempfile/temp3.dbf>' resize 30000M;
+
+-- create  tempfile
+alter tablespace temp add tempfile '<+DATA_IGC01P_DG04/igc01p03/tempfile/temp11>' size 60000M;
+
+
