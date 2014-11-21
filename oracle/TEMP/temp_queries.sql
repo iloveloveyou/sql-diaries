c9bf9e744914d614e106a905a922619244f26e6b
Roberto Reale <roberto.reale82@gmail.com>
2014-11-21 11:06:17 +0100

    Improvements.

--- oracle/TEMP/temp_queries.sql
+++ oracle/TEMP/temp_queries.sql
@@ -1,11 +1,11 @@
 -- 
--- Queries to list, create and extend TEMPShort description
+--  Size of temp files
 -- 
--- @tested on: 11g, 10g
+--  @tested on: 10g, 11g
 --
--- views: V$TEMPSEG_USAGE
 
--- List temp files
+
 set line 200
 col file_name format a80
+
 select file_name, tablespace_name, bytes/1024/1024 as "MBs" from dba_temp_files;
