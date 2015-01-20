2ed0d9f4b597353e36b3be6969e5f74d7d45ff0c
Matias Monteverde Giannini <matias.monteverde@gmail.com>
2015-01-20 12:13:17 +0100

    Create report_historic_usage_tbs.sql

---
+++ oracle/tablespaces/report_historic_usage_tbs.sql
@@ -0,0 +1,7 @@
+-
+-
+-
+-
+select NAME,RTIME,TABLESPACE_SIZE/1024/1024,TABLESPACE_MAXSIZE/1024/1024,TABLESPACE_USEDSIZE/1024/1024
+from dba_hist_tbspc_space_usage,v$tablespace where TABLESPACE_ID=TS# order by 1,2
+/
