6e521cd22c0ee30718e1e97da0ca6b2aeee0efe6
Matias Monteverde Giannini <matias.monteverde@gmail.com>
2015-01-20 12:14:24 +0100

    Update report_historic_usage_tbs.sql

--- oracle/tablespaces/report_historic_usage_tbs.sql
+++ oracle/tablespaces/report_historic_usage_tbs.sql
@@ -1,7 +1,8 @@
--
--
--
--
+-- 
+--Report historic tablespaces usage
+-- 
+-- @tested on: 10g, 11g
+
 select NAME,RTIME,TABLESPACE_SIZE/1024/1024,TABLESPACE_MAXSIZE/1024/1024,TABLESPACE_USEDSIZE/1024/1024
 from dba_hist_tbspc_space_usage,v$tablespace where TABLESPACE_ID=TS# order by 1,2
 /
