c22cb1bb85d0efe0d01439d285874697f0819f35
roberto.reale82@gmail.com <roberto.reale82@gmail.com>
2015-05-23 11:41:28 +0200

    Edited query.

--- oracle/tablespaces/total_and_used_percentage_of_tablespaces.sql
+++ oracle/tablespaces/total_and_used_percentage_of_tablespaces.sql
@@ -1,40 +1,34 @@
 --
--- @tested on: 10g, 11g
---
 -- Query to get total, used and free usage of tablesapces
 --
-
-column "Tablespace" format a30
-column "Used MB"    format 99,999,999
-column "Free MB"    format 99,999,999
-column "Total MB"   format 99,999,999
-col host_name format a20
-col instacen_name format a30
+-- @tested on: 10g
+--
+--
 
 
-select
-   fs.tablespace_name                          "Tablespace",
-   (df.totalspace - fs.freespace)              "Used MB",
-   fs.freespace                                "Free MB",
-   df.totalspace                               "Total MB",
-   round(100 * (fs.freespace / df.totalspace)) "Pct. Free"
-from
-   (select
-      tablespace_name,
-      round(sum(bytes) / 1048576) TotalSpace
-   from
-      dba_data_files where tablespace_name like 'SYS%'
-   group by
-      tablespace_name
-   ) df,
-   (select
-      tablespace_name,
-      round(sum(bytes) / 1048576) FreeSpace
-   from
-      dba_free_space where tablespace_name like 'SYS%'
-      group by
-      tablespace_name
-   ) fs
-where
-   df.tablespace_name = fs.tablespace_name order by "Pct. Free" Desc
-/
+SELECT
+       NVL(b.tablespace_name, NVL(a.tablespace_name, 'UNKOWN'))  tablespace_name,
+       kbytes_alloc                                              total,
+       kbytes_alloc - NVL(kbytes_free, 0)                        used,
+       NVL(kbytes_free, 0)                                       free,
+       ((kbytes_alloc - NVL(kbytes_free, 0))
+                             / kbytes_alloc) * 100               pct_used,
+       NVL(largest, 0)                                           largest
+
+  FROM
+      (SELECT SUM(bytes) / 1024                              kbytes_free,
+              MAX(bytes) / 1024                              largest,
+                                                             tablespace_name
+        FROM  sys.dba_free_space
+     GROUP BY tablespace_name) a
+  
+  LEFT JOIN
+      (SELECT SUM(bytes) / 1024                              kbytes_alloc,
+                                                             tablespace_name
+        FROM sys.dba_data_files
+     GROUP BY tablespace_name) b
+
+    ON a.tablespace_name = b.tablespace_name
+
+ORDER BY 1
+;
