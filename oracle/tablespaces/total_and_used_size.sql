2d871c136ab6d384b9d4594c4c0934f78672aa2f
roberto.reale82@gmail.com <roberto.reale82@gmail.com>
2015-05-23 11:43:44 +0200

    Fixed typo in query.

--- oracle/tablespaces/total_and_used_size.sql
+++ oracle/tablespaces/total_and_used_size.sql
@@ -7,24 +7,24 @@
 
 
 SELECT
-       NVL(b.tablespace_name, NVL(a.tablespace_name, 'UNKOWN'))  tablespace_name,
-       kbytes_alloc                                              total,
-       kbytes_alloc - NVL(kbytes_free, 0)                        used,
-       NVL(kbytes_free, 0)                                       free,
+       NVL(b.tablespace_name, NVL(a.tablespace_name, 'UNKNOWN'))  tablespace_name,
+       kbytes_alloc                                               total,
+       kbytes_alloc - NVL(kbytes_free, 0)                         used,
+       NVL(kbytes_free, 0)                                        free,
        ((kbytes_alloc - NVL(kbytes_free, 0))
-                             / kbytes_alloc) * 100               pct_used,
-       NVL(largest, 0)                                           largest
+                             / kbytes_alloc) * 100                pct_used,
+       NVL(largest, 0)                                            largest
 
   FROM
-      (SELECT SUM(bytes) / 1024                              kbytes_free,
-              MAX(bytes) / 1024                              largest,
-                                                             tablespace_name
+      (SELECT SUM(bytes) / 1024                               kbytes_free,
+              MAX(bytes) / 1024                               largest,
+                                                              tablespace_name
         FROM  sys.dba_free_space
      GROUP BY tablespace_name) a
   
   LEFT JOIN
-      (SELECT SUM(bytes) / 1024                              kbytes_alloc,
-                                                             tablespace_name
+      (SELECT SUM(bytes) / 1024                               kbytes_alloc,
+                                                              tablespace_name
         FROM sys.dba_data_files
      GROUP BY tablespace_name) b
 
