98aed628cb5dff56dfd94052333d574595ff2efd
Matias Monteverde Giannini <matias.monteverde@gmail.com>
2015-08-13 15:23:26 +0200

    Update gap_analisis.sql

--- oracle/dataguard/gap_analisis.sql
+++ oracle/dataguard/gap_analisis.sql
@@ -14,3 +14,14 @@ Count(1) Count#, Min(RECID) Min#, Max(RECID) Max# FROM gv$log_history GROUP BY T
 ORDER BY 1 DESC) A,(SELECT Avg(BYTES) AVG#, Count(1) Count#, Max(BYTES) Max_Bytes, 
 Min(BYTES) Min_Bytes FROM gv$log ) B;
 
+---
+Sample output:
+
+DAY                 COUNT#       MIN#       MAX# DAILY_AVG_MB
+--------------- ---------- ---------- ---------- ------------
+2015-08-13             524      33153      33286       131000
+2015-08-12             632      32995      33157       158000
+2015-08-11             600      32845      32999       150000
+2015-08-10             540      32710      32849       135000
+....
+
